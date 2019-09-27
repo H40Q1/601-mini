37cd3f38aec65595dc0e78d060bbe5a498cd4f6e
thaorell <khanhchuong7396@gmail.com>
2019-09-27 18:52:17 -0400

    functionalize NLTK

--- modules/google_module.py
+++ modules/google_module.py
@@ -6,38 +6,37 @@ from google.cloud.language import types
 client = language.LanguageServiceClient()
 
 # text
-text1 = u'test text here!'
-text2 = u'try the second one'
-text3 = u'I feel good today'
-texts=[]
-texts.append(text1)
-texts.append(text2)
-texts.append(text3)
-scores=[]
-for text in texts:
-    document = types.Document(
-        content=text,
-        type=enums.Document.Type.PLAIN_TEXT)
+def google_nltk(texts):
+    scores = []
+    for text in texts:
+        document = types.Document(
+            content=text,
+            type=enums.Document.Type.PLAIN_TEXT)
 
-    #sentiment feedback
-    sentiment = client.analyze_sentiment(document=document).document_sentiment
+        #sentiment feedback
+        sentiment = client.analyze_sentiment(document=document).document_sentiment
 
-    print('Text: {}'.format(text))
-    print("Sentiment Score:", sentiment.score)
- 
-    scores.append(sentiment.score)
+        print('Text: {}'.format(text))
+        print("Sentiment Score:", sentiment.score)
+    
+        scores.append(sentiment.score)
 
 
-    #sentiment level
-    if sentiment.score <=1 and sentiment.score>=0.7:
-        print("Sentiment level: Very Happy")
+        #sentiment level
+        if sentiment.score <=1 and sentiment.score>=0.7:
+            print("Sentiment level: Very Happy")
 
-    elif sentiment.score <0.7 and sentiment.score>=0.3:
-        print("Sentiment level: Happy")
+        elif sentiment.score <0.7 and sentiment.score>=0.3:
+            print("Sentiment level: Happy")
 
-    elif sentiment.score<0.3 and sentiment.score>=-0.3:
-        print("Sentiment level: OK")
+        elif sentiment.score<0.3 and sentiment.score>=-0.3:
+            print("Sentiment level: OK")
 
-    else:
-        print("Sentiment level: Disatified")
+        else:
+            print("Sentiment level: Disatified")
+    return scores
 
+if __name__ == '__main__':
+    #pass in the username of the account you want to download
+    google_nltk(["This is bad", "excellent", "iphone 11 is horrible"])
+    
